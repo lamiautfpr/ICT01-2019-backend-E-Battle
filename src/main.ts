b5c440768d0a3d05eb4d544795623d745aa51d5f
Guilherme-Veras <guilherme.2000@alunos.utfpr.edu.br>
2022-03-14 18:20:42 -0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Battle Api')
+    .setDescription('Api do projeto ICT01-2019 E-Battle')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
