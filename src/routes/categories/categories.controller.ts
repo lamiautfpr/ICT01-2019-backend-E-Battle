388cbb359bd38f676b321cf56c86d4cc1ef46661
Guilherme-Veras <guilherme.2000@alunos.utfpr.edu.br>
2022-03-28 23:02:22 -0300

    Lint

--- src/routes/categories/categories.controller.ts
+++ src/routes/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { CategoriesService } from './categories.service';
