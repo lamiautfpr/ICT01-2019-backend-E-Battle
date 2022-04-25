5cd77c3dc081c25cb001632596391a98d1533516
Gustavo_Martins <gtavomartins893@gmail.com>
2022-04-24 21:11:05 -0300

    Ceated Swagger description

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('E-Battle Api')
-    .setDescription('Api do backend do projeto ICT01-2019 E-Battle')
+    .setDescription('Api para integração de uma plataforma web com o projeto ICT01-2019 E-Battle que, por sua vez, é um jogo educacional que tem como objetivo estimular o aprendizado em sala de aula por meio da competição saudável entre os jogadores.')
     .setVersion('0.1.0')
     .addBearerAuth()
     .addTag('Development', 'APENAS PARA TESTES E DESENVOLVIMENTO')
