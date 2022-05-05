a899aa27321ac9a9a4d5fb2b8ee6887827f22cbf
Guilherme-Veras <guilherme.2000@alunos.utfpr.edu.br>
2022-05-05 16:15:16 -0300

    Lint

--- src/routes/games/games.service.ts
+++ src/routes/games/games.service.ts
@@ -11,15 +11,17 @@ export class GamesService {
   ) {}
 
   findByUser(userId: number): Promise<Game[] | undefined> {
-    return this.gamesRepository.find({ 
-      where: { user: userId }, 
+    return this.gamesRepository.find({
+      where: { user: userId },
       relations: ['language', 'category'],
     });
   }
 
   findOneByUser(id: number, userId: number): Promise<Game | undefined> {
-    return this.gamesRepository.findOne({ where: { id: id, user: userId }, 
-      relations: ['language', 'category'], });
+    return this.gamesRepository.findOne({
+      where: { id: id, user: userId },
+      relations: ['language', 'category'],
+    });
   }
 
   create(game: Game) {
