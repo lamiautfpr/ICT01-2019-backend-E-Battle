a43958742c3a2b21674b31a142116ce2ace02f68
Guilherme Veras <guilherme.veras2000@gmail.com>
2022-11-01 10:15:55 -0300

    Add log to default route

---
+++ src/_log/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { getConn } = require('/opt/nodejs/database');
+
+exports.handler = async (event) => {
+    const conn = await getConn();
+    await conn.query({
+        name: 'log',
+        text: 'INSERT INTO _log(`value`) VALUES ($1)',
+        values: [event],
+    });
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            status: 'working',
+        }),
+    };
+}
