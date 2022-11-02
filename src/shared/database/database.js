b31c4e192f405e9fe85aaabf3dce45af7b7d8366
Guilherme Veras <guilherme.veras2000@gmail.com>
2022-11-01 22:14:04 -0300

    Add env settings

--- src/shared/database/database.js
+++ src/shared/database/database.js
@@ -3,10 +3,10 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: 'ebattle',
-    host: 'lamiadb1.ceoy3jbkraru.us-east-1.rds.amazonaws.com',
-    database: 'ebattle',
-    password: '!M;,T?gO#Vkp',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: 5432,
 })
 
