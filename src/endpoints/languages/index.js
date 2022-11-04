8fcebb335a8065ebc6c11698426966f2e16c4f90
ErikHenrique09 <eriknascimento@alunos.utfpr.edu.br>
2022-11-04 15:37:31 -0300

    Mudança de criação de função no yaml e tambem, alterei as tags para Tags, e project para Project

---
+++ src/endpoints/languages/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { getConn } = require('/opt/nodejs/database');
+
+exports.handler = async () => {
+    const conn = await getConn();
+
+    const results = await conn.query({
+        name: "languagesget",
+        text: "SELECT id, name FROM languages ",
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(results.rows)
+    }
+}
