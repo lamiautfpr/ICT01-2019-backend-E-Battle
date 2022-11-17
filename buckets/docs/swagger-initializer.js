69b0a84767281f8f036b879fb22d1ac2fe32c20d
Guilherme Veras <guilherme.veras2000@gmail.com>
2022-11-17 18:13:55 -0300

    Update to new deploy

--- buckets/docs/swagger-initializer.js
+++ buckets/docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://api.ebattle.lamia-edu.com/dev/api",
+    url: "api",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
