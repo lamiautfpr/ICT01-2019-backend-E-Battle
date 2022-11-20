938e395ccd08a9815e3ba478c4b8d3a43cbbfc44
Guilherme Veras <guilherme.veras2000@gmail.com>
2022-11-20 17:16:43 -0300

    Update docs

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 exports.handler = async () => {
     return {
         statusCode: 200,
-        body: await fs.readFileSync('documentation.yaml', 'utf8'),
+        body: (await fs.readFileSync('documentation.yaml', 'utf8')).replace('{{env}}', process.env.ENVIRONMENT),
     };
 }
